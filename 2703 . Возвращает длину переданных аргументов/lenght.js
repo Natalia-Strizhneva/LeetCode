935321f91146deb1e1993f8093d5b27d1c20d6e2
Natalia-Strizhneva <homosum07@gmail.com>
2023-07-05 15:46:26 +0300

    first commit

---
+++ 2703 . Возвращает длину переданных аргументов/lenght.js
@@ -0,0 +1,10 @@
+/*Напишите функцию  argumentsLength, которая возвращает количество 
+переданных ей аргументов.*/
+
+function argumentsLength() {
+  return args.length;
+}
+
+let args = [{}, null, "3"];
+
+argumentsLength(); // 3
